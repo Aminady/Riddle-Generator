7c42d51e267e79f9dc8bac61faa6c6bb5e0153da
Aminady <aminasady56@gmail.com>
2022-11-19 19:31:13 +0000

    created some variables

---
+++ index.js
@@ -0,0 +1,4 @@
+const riddle = document.querySelector('.riddle');
+const answer = document.querySelector('.answer');
+const answerBtn = document.querySelector('.show-answer');
+const newRiddle = document.querySelector('.new-riddle');
