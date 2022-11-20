cdd8808224d38da6ccccd1c943ad431f792e17f4
Aminady <aminasady56@gmail.com>
2022-11-20 09:53:28 +0000

    created a function to fetch my API

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const riddle = document.querySelector('.riddle');
 const answer = document.querySelector('.answer');
 const answerBtn = document.querySelector('.show-answer');
 const newRiddle = document.querySelector('.new-riddle');
+
+function fetchAPI() { 
+    fetch('https://riddles-api.vercel.app/random', {mode : "cors"})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        riddle.textContent = response.riddle;
+        answerBtn.addEventListener('click', () => {
+        answer.textContent = response.answer;
+        })
+    });
+}
+
+fetchAPI()
